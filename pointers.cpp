f223895adaa081d50417ead88eb3e18c23891f4c
piechots <>
2021-11-11 16:12:55 +0000

    Initial commit

---
+++ pointers.cpp
@@ -0,0 +1,39 @@
+// Name:
+// Student Number:
+// Date:
+
+// Filename: pointers.cpp
+// Purpose: To work with pointers and dynamically allocate
+//           an array for the marks of a student as outlined
+//           in the exercise description.
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+struct Student
+{
+  string name;
+  int id;
+  int mark[3];
+};
+
+void inputStudent(Student* ptr);   // function prototype for inputting
+                                   // function prototype for printing
+
+//*********************** Main Function ************************//
+int main ()
+{
+  Student stu;		         // instantiating a Student object
+  Student* stuPtr = &stu;  // defining a pointer for the Student object
+
+  inputStudent(&stu);	     // inputting from the keyboard into the instance
+                     	     // printing the object
+
+
+
+  return 0;
+} // end of main
+
+//-----------------Start of functions----------------------------// 
